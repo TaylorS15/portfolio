36363ad6ac4ca7979a1a2aa0254850216eaf118e
Taylor Svec <taylorsvec978@yahoo.com>
2026-06-15 14:01:35 -0400

    ui: project name

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -13,13 +13,13 @@ export function ProjectCard({
     <div className="flex flex-row items-center justify-between gap-4">
       <div className="flex flex-row items-start gap-4">
         <p className="mt-1">*</p>
-        <p className="">{name}</p>
+        <p className="min-w-max">{name}</p>
         <p className="text-neutral-500">-</p>
         <p className="text-sm text-neutral-500">{description}</p>
       </div>
       {links.map((link, index) => (
         <a href={link} target="_blank" key={`${link}_${index}`}>
-          <ArrowUpRight />
+          <ArrowUpRight size={20} />
         </a>
       ))}
     </div>
