328db3367f37fed2baa19f86f33e4354da157af1
Taylor Svec <taylorsvec978@yahoo.com>
2026-06-15 22:31:45 -0400

    ui: added favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,6 +24,11 @@ export const Route = createRootRoute({
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico",
+      },
     ],
   }),
   shellComponent: RootDocument,
